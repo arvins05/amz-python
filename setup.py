a1d4a282b86fd86e1a3f122df41d6bce4215c255
arvin <arvins05@gmail.com>
2024-10-08 11:41:38 +0800

    update the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='zvamz',
     version='0.1',
+    packages=find_packages(),
     install_requires=[],
 )
